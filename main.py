00398124f1e4817f55a0e9c8b525449e177b9b4d
Deep1234y <99578701+Deep1234y@users.noreply.github.com>
2026-02-28 16:28:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -927,11 +927,11 @@ async def handle_message(update, context: ContextTypes.DEFAULT_TYPE):
         else:
             await update.message.reply_text("❌ Unsupported link.")
             return
-        if key:
-          await update.message.reply_text(
-    f"✅ Your Key:\n\n`{key}`",
-    parse_mode="Markdown"
-)
+                if key:
+            await update.message.reply_text(
+                f"✅ Your Key:\n\n`{key}`",
+                parse_mode="Markdown"
+            )
             asyncio.create_task(start_countdown(update, 180))
         else:
             await update.message.reply_text(f"❌ Failed:\n{error}")
@@ -957,6 +957,7 @@ def start_telegram_bot():
 
 if __name__ == "__main__":
     start_telegram_bot()
+
 
 
 
